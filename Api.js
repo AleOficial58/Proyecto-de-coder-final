546aacf880f651b9d4fbce8fbca5a25605e1becb
AleOficial58 <119017437+AleOficial58@users.noreply.github.com>
2023-03-21 01:59:14 -0300

    API Syntax errors and core issues
    API Syntax errors and core issues

--- Api.js
+++ Api.js
@@ -8,20 +8,20 @@ async function pruebaAPI(url){
     let response = await fetch(url);
     
 if (response.ok) { // response.ok == true
-    // get the response body (the method explained below)
-    // debugger //ACOSTRUMBRATE A DEBUGGUEAR
-    // await response.json()
-    // .then(resp => {
-    //     // console.log(resp)
-    //     if(resp != undefined && resp.country.length > 0){ //OJO CON ESTO, SI NO LO SABES SACALO
-    //         // console.log(resp.country)
-    //         resp.country.forEach(element => {
-    //             console.log(element)
-    //             console.log(element.country_id)
-    //             console.log(element.probability)
-    //         });
-    //     }
-    // })
+     //get the response body (the method explained below)
+     debugger //ACOSTRUMBRATE A DEBUGGUEAR
+     await response.json()
+     .then(resp => {
+         // console.log(resp)
+         if(resp != undefined && resp.country.length > 0){ //OJO CON ESTO, SI NO LO SABES SACALO
+            // console.log(resp.country)
+             resp.country.forEach(element => {
+                 console.log(element)
+                 console.log(element.country_id)
+                 console.log(element.probability)
+             });
+         }
+     })
   } else {
     alert("HTTP-Error: " + response.status);
   }
